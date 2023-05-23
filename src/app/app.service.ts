b3b8e34a24e6663218de84446ab82232e001b658
Kamil-Konieczny <kamil.konieczny4599@gmail.com>
2023-05-23 18:10:02 +0200

    final commit

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -18,10 +18,17 @@ export class AppService {
     return this.httpClient.post('http://localhost:8888/auth/login', payload)
   }
 
-  getDocumentsByEmail(token: string, email: string): Observable<Document[]> {
+  getDocumentsByEmail(token: string, email: string, keyword?: string, title?: string, minDate?: string, maxDate?: string, minWordsNumber?: string, maxWordsNumber?: string): Observable<Document[]> {
     this.header = this.header.set('Authorization', 'Bearer ' + token);
 
-    const params = new HttpParams().append('email',email);
+    const params = new HttpParams()
+        .append('email',email)
+        .append('keyword', keyword ? keyword : '')
+        .append('title', title ? title : '')
+        .append('minDate', minDate ? minDate : '')
+        .append('maxDate', maxDate ? maxDate : '')
+        .append('minWordsNumber', minWordsNumber ? minWordsNumber : '')
+        .append('maxWordsNumber', maxWordsNumber ? maxWordsNumber : '');
     return this.httpClient.get<Document[]>('http://localhost:8888/documents', {params, 'headers': this.header})
   }
 
@@ -32,5 +39,20 @@ export class AppService {
       (err) => console.error(err)
     );
   }
+  deleteDocument(token: string, id: string) {
+  this.header = this.header.set('Authorization', 'Bearer ' + token);
+  const options = { headers: this.header, params: { id: id } };
+  this.httpClient.post('http://localhost:8888/remove-document', null, options).subscribe(
+    (res) => console.log(res),
+    (err) => console.error(err)
+  );
+  }
 
+  downloadFile(token: string, id: string) {
+  console.log('Downloading file with id:', id);
+  this.header = this.header.set('Authorization', 'Bearer ' + token);
+  const options = { headers: this.header, params: { id: id }, responseType: 'blob' as const };
+  return this.httpClient.get('http://localhost:8888/download', options);
+  }
 }
+
--- src/app/components/documents-page/documents-page.component.ts
+++ src/app/components/documents-page/documents-page.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {AppService} from "../../app.service";
 import {Observable, of, switchMap} from "rxjs";
 import {ActivatedRoute, ParamMap, Router} from "@angular/router";
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { FileSaverService  } from 'ngx-filesaver';
+
 
 @Component({
   selector: 'app-documents-page',
@@ -11,10 +14,18 @@ import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 export class DocumentsPageComponent implements OnInit {
   documents!: Observable<Document[]>;
   private selectedFile!: File;
-
+  keyword: string = '';
+  divDisplay = 'none';
   constructor(private appService: AppService,
               private route: ActivatedRoute,
-              private router: Router) {
+              private router: Router,
+              private sanitizer: DomSanitizer,
+              private fileSaverService: FileSaverService) {
+  }
+
+  sanitizeSubtext(subtext: string): SafeHtml {
+    console.log(this.sanitizer.bypassSecurityTrustHtml(subtext));
+    return this.sanitizer.bypassSecurityTrustHtml(subtext);
   }
 
   ngOnInit(): void {
@@ -26,8 +37,58 @@ export class DocumentsPageComponent implements OnInit {
       });
     } else {
       this.router.navigate(['/']);
-    }
-  }
+    }}
+
+   getDocsByKeyword(keyword: string){
+    let email = localStorage.getItem("email");
+    let token = localStorage.getItem("token");
+    if (token && email) {
+     this.appService.getDocumentsByEmail(token, email, keyword).subscribe((res: any) => {
+        this.documents = of(res);
+      });
+    } else {
+    this.router.navigate(['/']);
+  }}
+
+  getDocsByTitle(title: string){
+    let email = localStorage.getItem("email");
+    let token = localStorage.getItem("token");
+    if (token && email) {
+     this.appService.getDocumentsByEmail(token, email, undefined, title).subscribe((res: any) => {
+        this.documents = of(res);
+      });
+    } else {
+    this.router.navigate(['/']);
+  }}
+
+  getDocsByDate(minDate: string, maxDate: string){
+  let email = localStorage.getItem("email");
+  let token = localStorage.getItem("token");
+  if (token && email) {
+     this.appService.getDocumentsByEmail(token, email, undefined, undefined, minDate, maxDate).subscribe((res: any) => {
+        this.documents = of(res);
+      });
+   } else {
+   this.router.navigate(['/']);
+  }}
+
+  getDocsByWordNumber(minWordsNumber: string, maxWordsNumber: string){
+  let email = localStorage.getItem("email");
+  let token = localStorage.getItem("token");
+  if (token && email) {
+  this.appService.getDocumentsByEmail(token, email, undefined, undefined, undefined, undefined, minWordsNumber, maxWordsNumber).subscribe((res: any) => {
+        this.documents = of(res);
+      });
+   } else {
+   this.router.navigate(['/']);
+  }}
+
+  showDiv() {
+ if (this.divDisplay === 'inline-block') {
+    this.divDisplay = 'none';
+  } else {
+    this.divDisplay = 'inline-block';
+  }}
 
   uploadFile(event: Event) {
     const target = event.target as HTMLInputElement;
@@ -36,15 +97,39 @@ export class DocumentsPageComponent implements OnInit {
       console.log(target.files[0]);
     }
   }
+
+  remove(document: Document) {
+   let token = localStorage.getItem("token");
+   if(token){
+     this.appService.deleteDocument(token, document.id);
+    }
+  setTimeout(() => {
+         window.location.reload();
+        }, 1000);
+  }
+
+  download(document : Document){
+     const token = localStorage.getItem('token');
+      if (token) {
+        this.appService.downloadFile(token, document.id).subscribe((response) => {
+          const blob = new Blob([response], { type: 'application/pdf' });
+          const filename = document.title+'.'+document.extension
+          this.fileSaverService.save(blob, filename);
+        });
+      }
+  }
+
   sendDocument() {
     let token = localStorage.getItem("token");
     let email = localStorage.getItem("email");
     const formData = new FormData();
     formData.append('file', this.selectedFile);
     if(token && email){
-      this.appService.sendDocument(token, email , formData);
+      this.appService.sendDocument(token, email, formData);
      }
-    window.location.reload();
+     setTimeout(() => {
+       window.location.reload();
+      }, 5000);
   }
 
 
@@ -57,14 +142,14 @@ export class DocumentsPageComponent implements OnInit {
       window.location.reload();
     });
   }
-
 }
 export interface Document
 {
+  id : string;
   email: string;
-  name: string;
+  title: string;
   extension: string;
   date: string;
   fileBytes: string;
-
+  subtext: string;
 }
